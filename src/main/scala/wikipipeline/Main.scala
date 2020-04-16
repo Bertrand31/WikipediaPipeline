086b7d9cc841d92892b7eab123c33b8cf4dbfbe0
Bertrand31 <bertrandjun@gmail.com>
2020-04-16 13:51:41 +0200

    Improve main logic, add logging, fix merging and filtering

--- src/main/scala/wikipipeline/Main.scala
+++ src/main/scala/wikipipeline/Main.scala
@@ -1,6 +1,6 @@
 package airportmatching
 
-import cats.Monoid
+import scala.util.chaining.scalaUtilChainingOps
 import cats.effect._
 import cats.implicits._
 import utils.FileUtils
@@ -8,6 +8,7 @@ import scala.collection.mutable.PriorityQueue
 import scala.util.hashing.MurmurHash3.stringHash
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import scala.collection.immutable.ArraySeq
 
 object Main extends IOApp {
 
@@ -17,14 +18,6 @@ object Main extends IOApp {
     def compare(a: Item, b: Item) = b._2 compare a._2
   }
 
-  implicit def itemQueueMonoid = new Monoid[PriorityQueue[Item]] {
-
-      override def empty: PriorityQueue[Item] = PriorityQueue.empty[Item](Ordering[Item])
-
-      override def combine(x: PriorityQueue[Item], y: PriorityQueue[Item]): PriorityQueue[Item] =
-        x ++ y
-  }
-
   private def parseLine(line: String): Option[Item] =
     line.split(" ") match {
        case Array(domain, title, count, _) => Some((domain + " " + title, count.toInt))
@@ -32,18 +25,22 @@ object Main extends IOApp {
     }
 
   private def getNMost(n: Int)(iter: Iterator[Item]): PriorityQueue[Item] = {
-    val nFirst = iter.take(n)
-    val heap = PriorityQueue.empty(ItemOrdering).addAll(nFirst)
-    iter
-      .foldLeft(heap)((heap, line) => {
-        val (_, count) = line
-        if (count <= heap.head._2) heap
-        else {
-          heap.dequeue
-          heap.enqueue(line)
-          heap
-        }
-      })
+    val baseHeap =
+      PriorityQueue
+        .empty(ItemOrdering)
+        .addAll(iter take n)
+    if (!iter.hasNext)
+      baseHeap
+    else
+      iter
+        .foldLeft(baseHeap)((heap, line) =>
+          if (line._2 <= heap.head._2)
+            heap
+          else
+            heap
+              .tap(_.dequeue)
+              .tap(_.enqueue(line))
+        )
   }
 
   private val hours: Seq[String] =
@@ -65,6 +62,16 @@ object Main extends IOApp {
 
   private val getDate: String => LocalDate = LocalDate.parse
 
+  private def ioPrint(str: String): IO[Unit] = IO { println(str) }
+
+  private def printResults(results: IndexedSeq[Item]): IO[Unit] = {
+    val str = results.map({
+      case (title, count) => s""""$title" was seen $count times\n"""
+    }).mkString
+    ioPrint(s"\n======= Top ${results.size} most seen pages =======\n") *>
+    ioPrint(str)
+  }
+
   def run(args: List[String]): IO[ExitCode] =
     args
       .map(getDate)
@@ -77,11 +84,9 @@ object Main extends IOApp {
           .map(getNMost(5))
       })
       .parSequence
-      .map(_.reduce(_ |+| _))
-      .map(_.dequeueAll.takeRight(5))
-      .map(count => {
-        println(count)
-        count
-      })
+      .map(_.map(_.dequeueAll.toMap))
+      .map(_ foldMap identity)
+      .map(_.to(ArraySeq).sortBy(- _._2).take(5))
+      .flatMap(printResults)
       .as(ExitCode.Success)
 }
